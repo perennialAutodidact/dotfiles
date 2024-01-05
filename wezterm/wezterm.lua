35300f7ce4b219cd5db964e4a9e723b0af87764e
Keegan Good <kgood@opb.org>
2024-01-05 10:57:14 -0800

    increased opacity

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require('wezterm');
+local wezterm = require("wezterm")
 
 local config = {}
 
@@ -7,31 +7,30 @@ local config = {}
 --    return config
 -- end
 
-wezterm.log_info('config file ' .. wezterm.config_file)
+wezterm.log_info("config file " .. wezterm.config_file)
 
 config.keys = {
-    {
-        key = 'r',
-        mods = 'CTRL|SHIFT',
-        action = wezterm.action.ReloadConfiguration,
-    },
-    {
-        key = 't',
-        mods = 'CTRL|SHIFT',
-        action = wezterm.action.SpawnTab 'CurrentPaneDomain'
-    },
+	{
+		key = "r",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ReloadConfiguration,
+	},
+	{
+		key = "t",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
 }
 
-config.default_domain = 'WSL:Ubuntu'
+config.default_domain = "WSL:Ubuntu"
 
 -- config.color_scheme = 'AdventureTime'
-config.color_scheme = 'Earthsong (Gogh)'
-config.window_background_opacity = 0.8
+config.color_scheme = "Earthsong (Gogh)"
+config.window_background_opacity = 0.9
 
 config.font = wezterm.font_with_fallback({
-    'Hack NFM',
-    'Fira Code'
+	"Hack NFM",
+	"Fira Code",
 })
-
 
 return config
