09f61f9445bf8d6cd3f11a896acc38b7ca5427bd
KG <perennialautodidact@gmail.com>
2024-03-10 09:59:08 -0700

    allow hidden files to be shown in find_file

--- nvim/lua/plugins/telescope/init.lua
+++ nvim/lua/plugins/telescope/init.lua
@@ -20,4 +20,22 @@ return {
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 	end,
+	config = function()
+		require("telescope").setup({
+			pickers = {
+				find_files = {
+					find_command = {
+						"rg",
+						"--no-ignore",
+						"--hidden",
+						"--files",
+						"-g",
+						"!**/node_modules/*",
+						"-g",
+						"!**/.git/*",
+					},
+				},
+			},
+		})
+	end,
 }
