1b1fec00b1d3d8218d75024305a36d144129e83c
KG <perennialautodidact@gmail.com>
2024-03-10 10:00:47 -0700

    fixed formatting

--- nvim/lua/plugins/linting/nvim-lint.lua
+++ nvim/lua/plugins/linting/nvim-lint.lua
@@ -4,13 +4,13 @@ if not ok then
 	return
 end
 
--- lint.linters_by_ft = {
--- javascript = { "eslint_d" },
--- typescript = { "eslint_d " },
--- }
+lint.linters_by_ft = {
+	javascript = { "eslint_d" },
+	typescript = { "eslint_d" },
+}
 
--- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
--- callback = function()
--- lint.try_lint()
--- end,
--- })
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	callback = function()
+		lint.try_lint()
+	end,
+})
